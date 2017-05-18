862c8199c55c0125d874415817f012e3d50441d6
JarninFang <jarnin.fang@servicenow.com>
2017-05-18 10:28:28 -0700

    Implemented async

--- script.js
+++ script.js
@@ -1,51 +1,134 @@
 var git = require('simple-git');
-var pathToRepo = __dirname + '/../../../git/platformml/glide';
+var async = require('async');
 
-git(pathToRepo).pull(function(err, update) {
-  console.log("Pulled from glide");
-  if(err) {
-    console.log("ERROR: " + JSON.stringify(err, null, 4));
-  } else {
-    console.log("UPDATE: " + JSON.stringify(update.summary, null, 4));
-  }
-});
 
-var pathToRepo = __dirname + '/../../../git/platformml/commons';
-git(pathToRepo).pull(function(err, update) {
-  console.log("Pulled from commons");
-  if(err) {
-    console.log("ERROR: " + JSON.stringify(err, null, 4));
-  } else {
-    console.log("UPDATE: " + JSON.stringify(update.summary, null, 4));
-  }
-});
 
-var pathToRepo = __dirname + '/../../../git/platformml/glide-test';
-git(pathToRepo).pull(function(err, update) {
-  console.log("Pulled from glide-test");
-  if(err) {
-    console.log("ERROR: " + JSON.stringify(err, null, 4));
-  } else {
-    console.log("UPDATE: " + JSON.stringify(update.summary, null, 4));
-  }
-});
 
-var pathToRepo = __dirname + '/../../../git/platformml/ml';
-git(pathToRepo).pull(function(err, update) {
-  console.log("Pulled from ml");
-  if(err) {
-    console.log("ERROR: " + JSON.stringify(err, null, 4));
-  } else {
-    console.log("UPDATE: " + JSON.stringify(update.summary, null, 4));
-  }
-});
+function pullFromCommons(callback) {
+  var pathToRepo = __dirname + '/../../../git/platformml/commons';
+  console.log("\n\nPULLING FROM commons-------------------------------------------------------------------------------");
+  git(pathToRepo).stash(function(err, update) {
+    if(!err) {
+      console.log("Stash results: " + update);
+      git(pathToRepo).pull(function(err, update) {
+        if(err) {
+          console.log("ERROR: " + JSON.stringify(err, null, 4));
+        } else {
+          console.log("PULL UPDATE: " + JSON.stringify(update.summary, null, 4));
+          git(pathToRepo).stash(['apply'], function(err, update) {
+            if(!err) {
+              console.log("STASH UPDATE: " + update);
+            }
+            callback(null, 'done');
+          });
+        }
+      });
+    }
+  });
+}
 
-var pathToRepo = __dirname + '/../../../git/platformml/ml-scheduler';
-git(pathToRepo).pull(function(err, update) {
-  console.log("Pulled from ml-scheduler");
-  if(err) {
-    console.log("ERROR: " + JSON.stringify(err, null, 4));
-  } else {
-    console.log("UPDATE: " + JSON.stringify(update.summary, null, 4));
-  }
-});
+function pullFromMl(callback) {
+  var pathToRepo = __dirname + '/../../../git/platformml/ml';
+  console.log("\n\nPULLING FROM ml-------------------------------------------------------------------------------");
+  git(pathToRepo).stash(function(err, update) {
+    if(!err) {
+      console.log("Stash results: " + update);
+      git(pathToRepo).pull(function(err, update) {
+        if(err) {
+          console.log("ERROR: " + JSON.stringify(err, null, 4));
+        } else {
+          console.log("PULL UPDATE: " + JSON.stringify(update.summary, null, 4));
+          git(pathToRepo).stash(['apply'], function(err, update) {
+            if(!err) {
+              console.log("STASH UPDATE: " + update);
+            }
+            callback(null, 'done');
+          });
+        }
+      });
+    }
+  });
+}
+function pullFromMlScheduler(callback) {
+  var pathToRepo = __dirname + '/../../../git/platformml/ml-scheduler';
+  console.log("\n\nPULLING FROM ml-scheduler-------------------------------------------------------------------------------");
+  git(pathToRepo).stash(function(err, update) {
+    if(!err) {
+      console.log("Stash results: " + update);
+      git(pathToRepo).pull(function(err, update) {
+        if(err) {
+          console.log("ERROR: " + JSON.stringify(err, null, 4));
+        } else {
+          console.log("PULL UPDATE: " + JSON.stringify(update.summary, null, 4));
+          git(pathToRepo).stash(['apply'], function(err, update) {
+            if(!err) {
+              console.log("STASH UPDATE: " + update);
+            }
+            callback(null, 'done');
+          });
+        }
+      });
+    }
+  });
+}
+
+function pullFromGlide(callback) {
+  var pathToRepo = __dirname + '/../../../git/platformml/glide';
+  console.log("\n\nPULLING FROM glide------------------------------------------------------------------------------");
+  git(pathToRepo).stash(function(err, update) {
+    if(!err) {
+      console.log("Stash results: " + update);
+      git(pathToRepo).pull(function(err, update) {
+        if(err) {
+          console.log("ERROR: " + JSON.stringify(err, null, 4));
+        } else {
+          console.log("PULL UPDATE: " + JSON.stringify(update.summary, null, 4));
+          git(pathToRepo).stash(['apply'], function(err, update) {
+            if(!err) {
+              console.log("STASH UPDATE: " + update);
+            }
+            callback(null, 'done');
+          });
+        }
+      });
+    }
+  });
+}
+
+function pullFromGlideTest(callback) {
+  var pathToRepo = __dirname + '/../../../git/platformml/glide-test';
+  console.log("\n\nPULLING FROM glide-test-------------------------------------------------------------------------------");
+  git(pathToRepo).stash(function(err, update) {
+    if(!err) {
+      console.log("Stash results: " + update);
+      git(pathToRepo).pull(function(err, update) {
+        if(err) {
+          console.log("PULL ERROR: " + JSON.stringify(err, null, 4));
+        } else {
+          console.log("PULL UPDATE: " + JSON.stringify(update.summary, null, 4));
+          git(pathToRepo).stash(['apply'], function(err, update) {
+            if(!err) {
+              console.log("STASH UPDATE: " + update);
+            }
+            callback(null, 'done');
+          });
+        }
+      });
+    }
+  });
+}
+
+
+
+
+async.series([
+  pullFromCommons,
+  pullFromMl,
+  pullFromGlide,
+  pullFromGlideTest,
+  pullFromMlScheduler
+  ], function(err, results) {
+    if(err)
+      console.log(err);
+    console.log('Done');
+  });
